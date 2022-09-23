85d1bcc923742043abc03734d110c0a972eae1a5
popaDaria <popadaria19@gmail.com>
2022-09-23 21:37:30 +0200

    changes localhost to deployed server url

--- src/util/Constants.tsx
+++ src/util/Constants.tsx
@@ -1,8 +1,8 @@
 const Constants = {
-	//BASE_URL : "https://tric-project.azurewebsites.net",
-	//SOCKET_URL: "https://tric-project.azurewebsites.net/ws-message"
-	BASE_URL: "http://localhost:8080",
-	SOCKET_URL: "http://localhost:8080/ws-message"
+	BASE_URL : "https://tric-project.azurewebsites.net",
+	SOCKET_URL: "https://tric-project.azurewebsites.net/ws-message"
+	//BASE_URL: "http://localhost:8080",
+	//SOCKET_URL: "http://localhost:8080/ws-message"
 };
 
 export default Constants;
