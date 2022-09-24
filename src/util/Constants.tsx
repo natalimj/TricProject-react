ecaf4329e0d4817ffd6ecf2ea2e71d8d469b049e
natalimj <61503435+natalimj@users.noreply.github.com>
2022-09-24 08:56:25 +0200

    Change server url

--- src/util/Constants.tsx
+++ src/util/Constants.tsx
@@ -1,6 +1,6 @@
 const Constants = {
-	BASE_URL : "https://tric-project.azurewebsites.net",
-	SOCKET_URL: "https://tric-project.azurewebsites.net/ws-message"
+	BASE_URL : "https://tricproject.azurewebsites.net",
+	SOCKET_URL: "https://tricproject.azurewebsites.net/ws-message"
 	//BASE_URL: "http://localhost:8080",
 	//SOCKET_URL: "http://localhost:8080/ws-message"
 };
