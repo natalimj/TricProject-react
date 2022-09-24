7c62ae3e953898d96945d242c6a9fb3ff79badc7
popaDaria <popadaria19@gmail.com>
2022-09-24 11:48:07 +0200

    added slash after base url

--- src/util/Constants.tsx
+++ src/util/Constants.tsx
@@ -1,7 +1,7 @@
 const Constants = {
 	BASE_URL : "https://tricproject.azurewebsites.net/",
 	SOCKET_URL: "https://tricproject.azurewebsites.net/ws-message"
-	//BASE_URL: "http://localhost:8080",
+	//BASE_URL: "http://localhost:8080/",
 	//SOCKET_URL: "http://localhost:8080/ws-message"
 };
 
