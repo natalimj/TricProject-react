30a108805732410fd3fdd443f48f758f885c601c
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-30 09:57:56 +0200

    fix

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -21,7 +21,7 @@ import {
 
 
 
-const StartPage: React.FC = () => {
+const StartPage = () => {
   const currentUser: IUserData = {
     userId: useAppSelector((state: RootState) => state.user.userId),
     username: useAppSelector((state: RootState) => state.user.username),
