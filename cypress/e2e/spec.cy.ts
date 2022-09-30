2079032030e0f9487a751b9c5f09194cefecb8cc
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-30 09:28:40 +0200

    workflow

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ describe('Test1', () => {
   it('Join', () => {
     cy.visit("https://jolly-forest-02e0b3603-8.westeurope.1.azurestaticapps.net");
     cy.get('[data-cy="join"]').click();
-    cy.get('[data-testid="create"]').should('be.visible');
+    cy.get('[data-testid="create"]',{timeout: 30000}).should('be.visible');
     cy.get('[data-testid="username"]').type("trashpanda");
     cy.get('[data-cy="create"]').click();
     cy.get('[data-testid="spinner"]').should('be.visible');
