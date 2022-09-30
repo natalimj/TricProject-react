4698cc810515a31a8b2b533f3130f44e262aac56
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-30 09:49:10 +0200

    remove empty file

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,8 @@
 describe('Test1', () => {
   it('Join', () => {
     cy.visit("https://jolly-forest-02e0b3603-8.westeurope.1.azurestaticapps.net");
-    cy.get('[data-cy="join"]').click();
-    cy.get('[data-testid="create"]',{timeout: 30000}).should('be.visible');
+    cy.get('[data-cy="join"]',{timeout: 30000}).click();
+    cy.get('[data-testid="create"]').should('be.visible');
     cy.get('[data-testid="username"]').type("trashpanda");
     cy.get('[data-cy="create"]').click();
     cy.get('[data-testid="spinner"]').should('be.visible');
