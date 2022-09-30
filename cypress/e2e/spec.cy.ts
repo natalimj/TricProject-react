b925e6eaee8ba605e8fb753813c8c2338e4a8606
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-30 10:49:52 +0200

    test fix

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,10 +2,10 @@ describe('Test1', () => {
   it('Join', () => {
     cy.visit("https://jolly-forest-02e0b3603-9.westeurope.1.azurestaticapps.net");
     cy.get('[data-cy="join"]').click();
-    cy.get('[data-testid="create"]').should('be.visible');
-    cy.get('[data-testid="username"]').type("trashpanda");
+    cy.get('[data-cy="create"]').should('be.visible');
+    cy.get('[data-cy="username"]').type("trashpanda");
     cy.get('[data-cy="create"]').click();
-    cy.get('[data-testid="spinner"]').should('be.visible');
+    cy.get('[data-cy="spinner"]').should('be.visible');
   })
 })
 
