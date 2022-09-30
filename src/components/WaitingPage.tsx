a921035d9ae8975879b951aba5c7774631a4f6e6
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-30 12:01:42 +0200

    spinner e2e id

--- src/components/WaitingPage.tsx
+++ src/components/WaitingPage.tsx
@@ -2,7 +2,7 @@ import { BeatLoader} from "react-spinners";
 
 const WaitingPage = () => {
   return (
-    <div style={{marginTop:"300px"}}>
+    <div style={{marginTop:"300px"}} data-cy="spinner">
       <BeatLoader color="#FFADCB" size={100} speedMultiplier={1}/>
       <h5> Please wait ...</h5>
     </div>
