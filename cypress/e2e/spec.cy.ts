36ee31f83b8436e065437c1266a9564d4dc78ff3
Bogdan Mezei <bogdan335@hotmail.com>
2022-09-30 10:45:34 +0200

    dynamic url

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('Test1', () => {
   it('Join', () => {
-    cy.visit("https://jolly-forest-02e0b3603-8.westeurope.1.azurestaticapps.net");
-    cy.get('[data-cy="join"]',{timeout: 30000}).click();
+    cy.visit("https://jolly-forest-02e0b3603-9.westeurope.1.azurestaticapps.net");
+    cy.get('[data-cy="join"]').click();
     cy.get('[data-testid="create"]').should('be.visible');
     cy.get('[data-testid="username"]').type("trashpanda");
     cy.get('[data-cy="create"]').click();
