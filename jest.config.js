da886521656a8f4cf29a07f36d3bd4fc7c4a7651
Bogdan Mezei <bogdan335@hotmail.com>
2022-10-17 13:39:53 +0200

    config fix

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  preset: 'jest-puppeteer',
+  transform: {"\\.ts$": ['ts-jest']},
   launch: {
     headless: process.env.CI === "true",
     ignoreDefaultArgs: ["--disable-extensions"],
