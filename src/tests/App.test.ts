a7ccf10b16673cebeb0ca618a49008c11a12424a
natalimj <natalimj88@gmail.com>
2022-12-09 12:46:29 +0100

    access token

--- src/tests/App.test.ts
+++ src/tests/App.test.ts
@@ -3,7 +3,7 @@ import AdminApi from "../api/AdminApi";
 import IQuestionData from "../models/Question";
 import IAnswerData from "../models/Answer";
 
-const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtb2QiLCJpYXQiOjE2NjkxMTg5MzYsImV4cCI6MTY2OTM3ODEzNn0.kSNuQCSWu027e_zEr5gCCVaN9QppDGTOwKGI4F0nUG5BaS-Y-2v1vk7MrGfWjIiVy9HQCC8saKPSdfrk_xMiQw";
+const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtb2QiLCJpYXQiOjE2NzA1ODU4ODYsImV4cCI6MTY3MDg0NTA4Nn0.npxyH8kr8ytt_kXSCbqfjUnjq0YXBm9PuaZBb3L7GIvcjTAcpo2okP9pczEIcbwqFe59TyPqdnODt-aDTv522w";
 let browser;
 let adminPage;
 let userPage;
