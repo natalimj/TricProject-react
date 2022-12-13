5bbc8ea6d9b1ce1203c5f3ffa8ca93aa83d5cb0e
natalimj <natalimj88@gmail.com>
2022-12-13 15:36:04 +0100

    token

--- src/tests/App.test.ts
+++ src/tests/App.test.ts
@@ -3,7 +3,7 @@ import AdminApi from "../api/AdminApi";
 import IQuestionData from "../models/Question";
 import IAnswerData from "../models/Answer";
 
-const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtb2QiLCJpYXQiOjE2NzA2NjI5NjcsImV4cCI6MTY3MDkyMjE2N30.QVqLx9iAxTOvUSD1yyTugD9Jws0jJ2yBau2F086SxlsKXTIfuHCiafBKV2mBUoQVImHR3frTU_eGla-koOLdzw";
+const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtb2QiLCJpYXQiOjE2NzA5NDIxMTIsImV4cCI6MTY3MTIwMTMxMn0.4Xe3ygUUxn8Yd-NKpA89o-AohuhwjWuRWwnGZtKcKe9ei8ghTeValS_hYGHZH44dM7aVCagE8F5paoSrfFJF-g";
 let browser;
 let adminPage;
 let userPage;
